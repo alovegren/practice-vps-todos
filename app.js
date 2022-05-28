23c2934e32be0d200f96c24d2bf7081443d8f347
Missy Lovegren <audrey.lovegren@gmail.com>
2022-05-28 13:49:15 -0500

    Serve index.html from express app

--- app.js
+++ app.js
@@ -2,9 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (_, response) => {
-  response.send("Hello World! This is Missy's Express app!");
-});
+app.use(express.static('public'));
 
 app.listen(port, () => {
 console.log(`Basic app listening on port ${port}`);
